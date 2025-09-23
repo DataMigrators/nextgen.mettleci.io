2956cc54df1512d8bdc680e9131b7216d1b20f1e
John McKeever <john.mckeever@datamigrators.com>
2025-09-23 20:35:08 +1000

    Deployed d83e3ce with MkDocs version: 1.6.1

--- railroads/namespace/namespace_overlay.py
+++ railroads/namespace/namespace_overlay.py
@@ -1,31 +1,26 @@
 import cairosvg
 from railroad import *
 
-DATASTAGE_COMPILE = Diagram(
+OVERLAY_APPLY = Diagram(
     NonTerminal('mcix'),
     Choice(0,
         'help',
         Sequence(
-            NonTerminal('datastage'),
+            NonTerminal('overlay'),
             Choice(0,
                 'help',
                 Sequence(
-                    NonTerminal('compile'),
+                    NonTerminal('apply'),
                     Choice(0, 
                         Terminal('help'),
                         Sequence(
                             MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-api-key'), '{api-key}'),
-                                Sequence(NonTerminal('-url'), '{URL}'),
-                                Sequence(NonTerminal('-user'), '{user}'),
-                                Sequence(NonTerminal('-report'), '{filename}')
+                                Sequence(NonTerminal('-assets'), '{api-key}'),
+                                Sequence(NonTerminal('-ouptut'), '{directory-or-filename}'),
+                                Sequence(NonTerminal('-overlay'), '{directory}')
                             ),
                             Choice(0,
-                                Sequence(NonTerminal('-project'), '{project-name}'),
-                                Sequence(NonTerminal('-proect-id'), '{project-id}')
-                            ),
-                            Optional(
-                                NonTerminal('-include-job-in-test-name')
+                                Sequence(NonTerminal('-properties'), '{filename}')
                             )
                         )
                     )
@@ -35,288 +30,7 @@ DATASTAGE_COMPILE = Diagram(
     )
 )
 
-DATASTAGE_IMPORT = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0,
-                'help',
-                Sequence(
-                    NonTerminal('import'),
-                    Choice(0, 
-                        Terminal('help'),
-                        Sequence(
-                            MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-api-key'), '{api-key}'),
-                                Sequence(NonTerminal('-url'), '{URL}'),
-                                Sequence(NonTerminal('-user'), '{user}'),
-                                Sequence(NonTerminal('-assets'), '{file-pattern}')
-                            ),
-                            Choice(0,
-                                Sequence(NonTerminal('-project'), '{project-name}'),
-                                Sequence(NonTerminal('-proect-id'), '{project-id}')
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-
-DATASTAGE_CAPTURE = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0, 
-                Terminal('help'),
-                Sequence(
-                    NonTerminal('capture'), 
-                    Choice(0,
-                        'help',
-                        Sequence(
-                            MultipleChoice(0, 'all',
-                                Sequence(NonTerminal('-domain'), '{service tier}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-username'), '{username}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-location'), '{directory}'),
-                                Sequence(NonTerminal('-project-cache'), '{directory}')
-                            ),
-                            MultipleChoice(0, 'any',
-                                Sequence(Skip()), 
-                                Sequence(NonTerminal('-threads'), '{number}')
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-DATASTAGE_CCMT = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0, 
-                Terminal('help'),
-                Sequence(
-                    NonTerminal('ccmt'), 
-                                        Choice(0, 
-                        Terminal('help'),
-                        Sequence(
-                            MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-domain'), '{domain}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-username'), '{user}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                                Sequence(NonTerminal('-logfile'), '{filename}'),
-                                Sequence(NonTerminal('-isxdirectory'), '{directory}'),
-                            ),
-                            Optional(
-                                Choice(0, 
-                                    Sequence(NonTerminal('-param'), '{parameter}'),
-                                    Sequence(NonTerminal('-project-cache'), '{directory}'),
-                                    Sequence(NonTerminal('-threads'), '{number}'),
-                                    Sequence(NonTerminal('-heapsize'), '{number}'),
-                                    Sequence(NonTerminal('-noBatchThreshold'), '{path}'),
-                                ),
-                                'skip'
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-DATASTAGE_CLEANUP_PROJECTS = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0, 
-                Terminal('help'),
-                Sequence(
-                    NonTerminal('cleanup-projects'), 
-                                        Choice(0, 
-                        Terminal('help'),
-                        Sequence(
-                            MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-domain'), '{domain}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-username'), '{user}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                                Sequence(NonTerminal('-pattern'), '{filename}'),
-                            ),
-                            Optional(
-                                Choice(0, 
-                                Sequence(NonTerminal('-retain'), '{number}'),
-                                ),
-                                'skip'
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-DATASTAGE_CREATE_PROJECT = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0,
-                'help',
-                Sequence(
-                    NonTerminal('create-project'),
-                    Choice(0, 
-                        Terminal('help'),
-                        Sequence(
-                            MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-domain'), '{domain}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-username'), '{user}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                            ),
-                            MultipleChoice(0, 'any',
-                                Sequence(Skip()),
-                                Sequence(NonTerminal('-path'), '{path}')
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-
-
-DATASTAGE_DELETE_PROJECT = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0,
-                'help',
-                Sequence(
-                    NonTerminal('delete-project'),
-                    Choice(0, 
-                        Terminal('help'),
-                        Sequence(
-                            MultipleChoice(0, 'all', 
-                                Sequence(NonTerminal('-domain'), '{domain}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-username'), '{user}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-DATASTAGE_DEPLOY = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0, 
-                Terminal('help'),
-                Sequence(
-                    NonTerminal('deploy'), 
-                    Choice(0,
-                        'help',
-                        Sequence(
-                            MultipleChoice(0, 'all',
-                                Sequence(NonTerminal('-domain'), '{service tier}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-username'), '{username}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-assets'), '{directory}'),
-                                Sequence(NonTerminal('-project-cache'), '{directory}')
-                            ),
-                            MultipleChoice(0, 'any',
-                                Sequence(Skip()), 
-                                Sequence(NonTerminal('-include-job-in-test-name')), 
-                                Sequence(NonTerminal('-threads'), '{number}'),
-                                Sequence(NonTerminal('-parameter-sets'), '{directory}')
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
-
-DATASTAGE_EXECUTE = Diagram(
-    NonTerminal('mcix'),
-    Choice(0,
-        'help',
-        Sequence(
-            NonTerminal('datastage'),
-            Choice(0, 
-                Terminal('help'),
-                Sequence(
-                    NonTerminal('execute'), 
-                    Choice(0,
-                        'help',
-                        Sequence(
-                            MultipleChoice(0, 'all',
-                                Sequence(NonTerminal('-domain'), '{service tier}'),
-                                Sequence(NonTerminal('-server'), '{engine tier}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-username'), '{username}'),
-                                Sequence(NonTerminal('-password'), '{password}'),
-                                Sequence(NonTerminal('-project'), '{project}'),
-                                Sequence(NonTerminal('-jobname'), '{job}')
-                            ),
-                            MultipleChoice(0, 'any',
-                                Sequence(Skip()), 
-                                Sequence(NonTerminal('-runmode'), 'NORMAL|RESET|RESTART|VALIDATE'),
-                                Sequence(NonTerminal('-param'), '{parameter}')
-                            )
-                        )
-                    )
-                )
-            )
-        )
-    )
-)
 
 datastage = {
-    'datastage-compile': DATASTAGE_COMPILE,
-    'datastage-import': DATASTAGE_IMPORT,
-    'datastage-capture': DATASTAGE_CAPTURE,
-    'datastage-ccmt': DATASTAGE_CCMT,
-    'datastage-cleanup-projects': DATASTAGE_CLEANUP_PROJECTS,
-    'datastage-create-project': DATASTAGE_CREATE_PROJECT,
-    'datastage-delete-project': DATASTAGE_DELETE_PROJECT,
-    'datastage-deploy': DATASTAGE_DEPLOY,
-    'datastage-execute': DATASTAGE_EXECUTE
+    'overlay-apply': OVERLAY_APPLY
 }
